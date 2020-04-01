1355208730fbf2c8d11496c4dbc4046f9976756c
caishuxing <caishuxing@linggan.com>
2020-04-01 10:22:25 +0800

    接入arouter

---
+++ app/src/main/java/com/easy/apt/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.easy.apt;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+
+@Route(path = "/app/HomeActivity")
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void goMain(View view) {
+        ARouter.getInstance().build("/app/MainActivity").navigation();
+    }
+}
--- app/src/main/java/com/easy/apt/MainActivity.java
+++ app/src/main/java/com/easy/apt/MainActivity.java
@@ -3,12 +3,21 @@ package com.easy.apt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+
+@Route(path = "/app/MainActivity")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goHome(View view) {
+        ARouter.getInstance().build("/app/HomeActivity").navigation();
     }
 }
